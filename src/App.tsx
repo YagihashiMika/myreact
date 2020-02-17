1a384b74408af0bccdad7c74b78c44595dcccfa3
やぎはしみか <yagihashimika@yagi.local>
2020-02-17 15:06:08 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const msgStyle = {
+  color: 'blue'
+};
+
+const App = () => {
+  return (
+    <>
+      <h1 style={msgStyle}>はじめてのReact</h1>
+    </>
+  );
+}
+
+
+export default App;
