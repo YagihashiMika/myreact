a2f765aac3a387f81be6e0b7e40c255ad3404cee
YagihashiMika <yagihashi.w@gmail.com>
2020-03-24 23:26:29 +0900

    Updates

---
+++ precache-manifest.3d2f8b93d779bb74fee9d23b965b06e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6912ad2bfb98980db626de985485d82",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "93e36bc17e0f55fd9e6d",
+    "url": "/myreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e49990da14a73c145e53",
+    "url": "/myreact/static/js/2.ed918265.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myreact/static/js/2.ed918265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e36bc17e0f55fd9e6d",
+    "url": "/myreact/static/js/main.499b0230.chunk.js"
+  },
+  {
+    "revision": "94c2d0b91ba7f2c63174",
+    "url": "/myreact/static/js/runtime-main.731f865f.js"
+  }
+]);
